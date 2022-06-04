ce248cea3d0d45214b74c23ecfe625686a2ceb30
Noirkun <noirkun2306@gmail.com>
2022-06-04 18:50:56 +0900

    wip

--- Assets/Scripts/Kikuchi/DontDestroy.cs
+++ Assets/Scripts/Kikuchi/DontDestroy.cs
@@ -6,8 +6,10 @@ public class DontDestroy : MonoBehaviour
 {
     void Start()
     {
+        SceneController.Instance.sceneChange("FirstScene");
         DontDestroyOnLoad(gameObject);
     }
 }
+
 
 
--- Assets/Scripts/Kikuchi/SceneController.cs
+++ Assets/Scripts/Kikuchi/SceneController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using System.Threading.Tasks;//追加
 using UnityEngine.SceneManagement;//追加
 
-public class SceneController : MonoBehaviour
+public class SceneController : SingletonMonoBehaviour<SceneController>
 {
     public GameObject fade;//インスペクタからPrefab化したCanvasを入れる
-    public GameObject fadeCanvas;//操作するCanvas、タグで探す
+    public FadeManager fadeCanvas;//操作するCanvas、タグで探す
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
 
@@ -15,21 +15,24 @@ public class SceneController : MonoBehaviour
     {
         Debug.Log("できたよ");
         SceneManager.LoadScene("FirstScene",LoadSceneMode.Additive);
-        //if (!FadeManager.isFadeInstance)//isFadeInstanceは後で用意する
-        //{
-        //    Instantiate(fade);
-        //}
-        //Invoke("findFadeObject", 0.02f);//起動時用にCanvasの召喚をちょっと待つ
+    }
+    void Start()
+    {
+        if (!FadeManager.isFadeInstance)//isFadeInstanceは後で用意する
+        {
+            Instantiate(fade);
+        }
+        Invoke("findFadeObject", 0.02f);//起動時用にCanvasの召喚をちょっと待つ
     }
     void findFadeObject()
     {
         //fadeCanvas = GameObject.FindGameObjectWithTag("Fade");//Canvasをみつける
-        fadeCanvas.GetComponent<FadeManager>().fadeIn();//フェードインフラグを立てる
+        fadeCanvas.fadeIn();//フェードインフラグを立てる
     }
 
     public async void sceneChange(string PreviewScene)//ボタン操作などで呼び出す
     {
-        fadeCanvas.GetComponent<FadeManager>().fadeOut();//フェードアウトフラグを立てる
+        fadeCanvas.fadeOut();//フェードアウトフラグを立てる
         await Task.Delay(200);//暗転するまで待つ
         SceneManager.LoadScene(PreviewScene);//シーンチェンジ
     }
